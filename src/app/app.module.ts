daf5912fadf589a5f13638506bb6820fcfcf04d0
Sergey Klevakin <justerest@yandex.ru>
2018-01-04 09:20:12 +0500

    theme does not work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { MatInputModule, MatSidenavModule } from '@angular/material';
 import { NgModule } from '@angular/core';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule,
+    MatSidenavModule,
+    NoopAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
