ec5b00e07f9ef0471d770181a0ae8b40dbc8444d
Sergey Klevakin <justerest@yandex.ru>
2018-01-22 15:10:28 +0500

    feat: add offline mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+import { environment } from 'environments/environment';
+
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChartsModule } from './charts/charts.module';
 import { CoreModule } from './core/core.module';
 import { GamesModule } from './games/games.module';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,6 +24,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CoreModule,
     GamesModule,
     HttpClientModule,
+    ServiceWorkerModule.register('/school/ngsw-worker.js', { enabled: environment.production }),
   ],
   bootstrap: [AppComponent],
 })
