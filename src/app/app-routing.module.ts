6a46f54059ce0d2be6afd138458705e4123b5074
Sergey Klevakin <justerest@yandex.ru>
2018-01-23 22:24:08 +0500

    style: sort imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ChartsComponent } from './charts/charts.component';
 import { MainPageComponent } from './core/main-page.component';
 
