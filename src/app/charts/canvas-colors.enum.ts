93e3cb7c0ce301a191fb29e62d1c63fb10adfcf3
Sergey Klevakin <justerest@yandex.ru>
2018-02-07 22:09:23 +0500

    refactor(charts): structure

---
+++ src/app/charts/canvas-colors.enum.ts
@@ -0,0 +1,8 @@
+/** Используемые цвета */
+export enum CanvasColors {
+  blue = '#009bff',
+  boldPencil = '#505050',
+  darkBlue = '#000aff',
+  orange = '#f44336',
+  pencil = '#737373',
+}
---
+++ src/app/charts/chart-types.enum.ts
@@ -0,0 +1,6 @@
+/** Разработанные функции */
+export enum ChartTypes {
+  linear = 'linear',
+  parabole = 'parabole',
+  hyperbole = 'hyperbole',
+}
