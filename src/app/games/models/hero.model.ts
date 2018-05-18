c5c70b04b98939cc377bc305ae16e35865a60a97
Sergey Klevakin <justerest@yandex.ru>
2018-05-18 21:47:26 +0500

    fix: delete flappy bird effect for hero

--- src/app/games/models/hero.model.ts
+++ src/app/games/models/hero.model.ts
@@ -8,6 +8,7 @@ export class HeroModel extends DrawedImage {
 
   /** Максимальное ускорение героя. */
   static ACCELERATION_MAX = 8;
+
   /** Ускорение героя за один шаг. */
   static ACCELERATION_STEP = 0.5;
 
@@ -15,23 +16,23 @@ export class HeroModel extends DrawedImage {
   acceleration = 1;
 
   accelerate(keys: KeyboardControlService['keys'], gameSpeed: GameSpeed) {
-    const isAcceleration = (
-      (this._getAxisSum(keys, 'x') || this._getAxisSum(keys, 'y')) &&
-      this.acceleration - gameSpeed.value < HeroModel.ACCELERATION_MAX
-    );
-    if (isAcceleration) this.acceleration += HeroModel.ACCELERATION_STEP;
-    else this.acceleration = 1;
+    if (!this.getAxisSum(keys, 'x') && !this.getAxisSum(keys, 'y')) {
+      this.acceleration = 1;
+    }
+    else if (this.acceleration - gameSpeed.value < HeroModel.ACCELERATION_MAX) {
+      this.acceleration += HeroModel.ACCELERATION_STEP;
+    }
 
     return this;
   }
 
   moveByControl(keys: KeyboardControlService['keys']) {
-    this.move(this._getAxisSum(keys, 'x'), this._getAxisSum(keys, 'y'));
+    this.move(this.getAxisSum(keys, 'x'), this.getAxisSum(keys, 'y'));
 
     return this;
   }
 
-  private _getAxisSum(keys: KeyboardControlService['keys'], axis: 'x' | 'y' | 'z') {
+  private getAxisSum(keys: KeyboardControlService['keys'], axis: 'x' | 'y' | 'z') {
     const dPlus = toInt(axis === 'x' ? keys.right : keys.down);
     const dMinus = toInt(axis === 'x' ? keys.left : keys.up);
     let movement = dPlus - dMinus;
